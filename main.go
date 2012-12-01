a4fd63ecfaf5a055aaac0cd2aa1307bee29eaf61
Steve McCoy <mccoyst@gmail.com>
2012-12-01 12:46:30 -0500

    Use a word-in-line address that works. Work for #1.

--- main.go
+++ main.go
@@ -3,9 +3,9 @@
 /*
 The 9spell command runs the plan9port "9 spell" program on each
 of the files supplied as arguments. It prints output like:
-	file0:45:/teh/
-	file0:64:/frgo/
-	file1:3:/fner/
+	file0:45-/^/+/teh/
+	file0:64-/^/+/frgo/
+	file1:3-/^/+/fner/
 A program such as acme can read those addresses and navigate
 to the misspelled word.
 
@@ -63,7 +63,7 @@ func check(file string) {
 
 		for _, w := range strings.FieldsFunc(trim(line), isWordSep) {
 			if typos[w] {
-				fmt.Printf("%s:%d:/%s/\n", file, n, w)
+				fmt.Printf("%s:%d-/^/+/%s/\n", file, n, w)
 			}
 		}
 		n++
